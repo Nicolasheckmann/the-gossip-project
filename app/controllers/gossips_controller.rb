587a9a74acc6d725f93f39bbaf53ec3913f88b94
Nicolasheckmann <nicolas.heckman@gmail.com>
2021-05-12 19:10:11 +0200

    added a destroy gossip functionnality

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,19 +1,47 @@
 require 'pry'
 class GossipsController < ApplicationController
+
   def index
     @gossips = Gossip.all
   end
+
   def show
     @gossip = Gossip.find(params[:id])
   end
+
   def new
   end
+
   def create
     @gossip = Gossip.new(title: params[:title], content: params[:content], user: User.where(email: "anonymous@anonymous.com").first)
     if @gossip.save
-      redirect_to gossips_path(title: params[:title])
+      flash[:notice] = 'Nouveau Gossip créé'
+      redirect_to gossips_path
     else
+      flash.now[:notice] = "Nous n'avons pas réussi à créer le nouveau Gossip pour la (ou les) raison(s) suivante(s) :"
       render 'new'
     end
+  end
+
+  def edit
+    @gossip = Gossip.find(params[:id])
+  end
+
+  def update
+    @gossip = Gossip.find(params[:id])
+    if @gossip.update(title: params[:gossip][:title], content: params[:gossip][:content] )
+      flash[:notice] = 'Gossip modifié'
+      redirect_to @gossip
+    else
+      flash.now[:notice] = "Nous n'avons pas réussi à modifier le Gossip pour la (ou les) raison(s) suivante(s) :"
+      render :edit
+    end
+  end
+
+  def destroy
+    @gossip = Gossip.find(params[:id])
+    @gossip.destroy
+    flash[:notice] = 'Gossip détruit'
+    redirect_to gossips_path
   end
 end
