9631a033d7d3ff241b05b33c4110640e4e59039a
Nicolasheckmann <nicolas.heckman@gmail.com>
2021-05-13 22:56:24 +0200

    improved likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,11 @@
 class LikesController < ApplicationController
   before_action :authenticate_user
-  
+
   def create
     @gossip = Gossip.find(params[:gossip_id])
     @like = Like.new(gossip: @gossip, user: current_user)
     if @like.save
-      flash[:notice] = ' Gossip liké'
+      flash[:notice] = 'Gossip liké'
       redirect_back(fallback_location: root_path)
     else
       flash.now[:notice] = "Nous n'avons pas réussi à liker le Gossip pour la (ou les) raison(s) suivante(s) :"
